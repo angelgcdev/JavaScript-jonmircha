55a7abbd13eec0274cda1589a90c3545a7e67c5c
angelgcdev <angelgcdev@gmail.com>
2023-08-19 18:05:01 -0400

    constantes en javascript

--- 00_basicos/00_basicos.js
+++ 00_basicos/00_basicos.js
@@ -1,27 +1,50 @@
-var hola = "Hola Mundo";
-let hello = "Hello World";
-console.log(hola);
-console.log(hello);
-console.log(window);
-console.log(window.hola);
-console.log(window.hello);
+// var hola = "Hola Mundo";
+// let hello = "Hello World";
+// console.log(hola);
+// console.log(hello);
+// console.log(window);
+// console.log(window.hola);
+// console.log(window.hello);
 
-console.log("**********var**********");
-var musica = "Rock";
-console.log(`Variable Música antes del Bloque ${musica}`);
+// console.log("**********var**********");
+// var musica = "Rock";
+// console.log(`Variable Música antes del Bloque ${musica}`);
 
-{
-  var musica = "Pop";
-  console.log(`Variable Música dentro del Bloque ${musica}`);
-}
+// {
+//   var musica = "Pop";
+//   console.log(`Variable Música dentro del Bloque ${musica}`);
+// }
 
-console.log(`Variable Música despues del Bloque ${musica}`);
+// console.log(`Variable Música despues del Bloque ${musica}`);
 
-console.log("**********let**********");
-let musica2 = "Rock";
-console.log(`Variable Música antes del Bloque ${musica2}`);
-{
-  let musica2 = "Pop";
-  console.log(`Variable Mùsica dentro del Bloque ${musica2}`);
-}
-console.log(`Variable Mùsica despues del Bloque ${musica2}`);
+// console.log("**********let**********");
+// let musica2 = "Rock";
+// console.log(`Variable Música antes del Bloque ${musica2}`);
+// {
+//   let musica2 = "Pop";
+//   console.log(`Variable Mùsica dentro del Bloque ${musica2}`);
+// }
+// console.log(`Variable Mùsica despues del Bloque ${musica2}`);
+let a;
+const PI = 3.141516;
+console.log(PI);
+// PI = 1.345;
+
+a = "kenai";
+console.log(a);
+
+const objeto = {
+  nombre: "Jon",
+  edad: 35,
+};
+
+const colores = ["rojo", "azul", "verde"];
+
+console.log(objeto);
+console.log(colores);
+
+objeto.correo = "jonmircha@gmail.com";
+colores.push("naranja");
+
+console.log(objeto);
+console.log(colores);
